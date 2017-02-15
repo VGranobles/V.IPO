b7d8ec000b0d027e7f15c9b75e9de5b0bbac36dc
Your Name <you@example.com>
2017-02-15 10:37:43 -0500

    cambio1

---
+++ a.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+	int valor = 0;
+	cout << "ingrese un valor" <<endl;
+	cin >> valor;
+	cout << "el valor ingresado es "<<valor<<endl;
+	cout << (valor < 5.75)<<endl;
+	cout << ((valor*valor) < (3*valor))<<endl;
+}
